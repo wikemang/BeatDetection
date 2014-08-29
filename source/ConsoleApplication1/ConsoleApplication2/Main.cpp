a3160fe902fe59982b37040eb76a15f85a227804
wikemang <mikescorpian@hotmail.com>
2014-08-29 15:17:58 -0400

    test 2

--- source/ConsoleApplication1/ConsoleApplication2/Main.cpp
+++ source/ConsoleApplication1/ConsoleApplication2/Main.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include "CImg.h"
+#include "FreeImage.h"
 
 using namespace std;
-using namespace cimg_library;
 
 int main(){
+	/*
 	ifstream infile("../../../sample-data.txt");
 	string line;
 	double left, right;
@@ -29,18 +29,8 @@ int main(){
 			cout << left << " " << right << endl;
 		}
 		avg.push_back((left + right) / 2);
-	}
-	CImg<unsigned char> visu(4000, 200, 1, 3, 0);
-	const unsigned char red[] = { 255, 0, 0 }, green[] = { 0, 255, 0 }, blue[] = { 0, 0, 255 };
-	CImgDisplay draw_disp(visu, "Intensity profile");
-	visu.fill(0);
-	//visu.fill(0).draw_graph(image.get_crop(0, y, 0, 0, image.width() - 1, y, 0, 0), red, 1, 1, 0, 255, 0);
-	//visu.draw_graph(image.get_crop(0, y, 0, 1, image.width() - 1, y, 0, 1), green, 1, 1, 0, 255, 0);
-	//visu.draw_graph(image.get_crop(0, y, 0, 2, image.width() - 1, y, 0, 2), blue, 1, 1, 0, 255, 0).display(draw_disp);
-	for (int i = 0; i < avg.size(); i++){
-		visu.draw_point(i, int(avg[i] + 175), red).display(draw_disp);
-		cout << avg[i] << endl;
-	}
-	visu.save("asdf.bmp", -1, 6);
+	}*/
+	FIBITMAP *dib = FreeImage_Load(FIF_BMP, "asdf.bmp", BMP_DEFAULT);
+	FreeImage_Unload(dib);
 	return 0;
 }

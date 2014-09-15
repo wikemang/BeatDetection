5e7cb38b0e72f9be95c34d5475ddfd045d0bff8d
wikemang <mikescorpian@hotmail.com>
2014-09-15 17:16:26 -0400

    render partial waveform

--- source/ConsoleApplication1/ConsoleApplication2/Main.cpp
+++ source/ConsoleApplication1/ConsoleApplication2/Main.cpp
@@ -2,53 +2,151 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <windows.h>
 #include "FreeImage.h"
 
 using namespace std;
 
-int main(){
-	/*
-	ifstream infile("../../../sample-data.txt");
-	string line;
-	double left, right;
-	vector<double> avg;
-	getline(infile, line);
-	while (line.substr(0, 6) != "[-inf]"){
-		getline(infile, line);
+short getnum(char lsb, char msb){
+	short num = (msb << 8) | lsb;
+	return num;
+}
+
+void fillcurve(int dots[1000]){
+	int silence_duration = 0;
+	int previous_dot = 0;
+	int inc;
+	for (int i = 0; i < 1000; i++){
+		if (dots[i] != 0){
+			if (silence_duration == 0){
+				previous_dot = dots[i];
+				continue;
+			}
+			inc = (dots[i] - previous_dot) / silence_duration;
+			for (int j = i - silence_duration; j < i; j++){
+				previous_dot += inc;
+				dots[j] = previous_dot;
+			}
+			previous_dot = dots[i];
+			silence_duration = 0;
+		}
+		else{
+			silence_duration++;
+		}
 	}
-	for (int i = 0; i < 4000; i++){
-		getline(infile, line);
-		size_t loc = line.find("\t");
-		if (loc != string::npos) {
-			left = atof(line.substr(0, loc).c_str());
-			right = atof(line.substr(loc + 1).c_str());
+}
+
+void drawCurve(FIBITMAP *dib, int dot[1000], int width, int height){
+	RGBQUAD blue;
+	blue.rgbBlue = 255;
+	blue.rgbGreen = 100;
+	blue.rgbRed = 100;
+	blue.rgbReserved = 125;
+	RGBQUAD black;
+	black.rgbBlue = 0;
+	black.rgbGreen = 0;
+	black.rgbRed = 0;
+	black.rgbReserved = 125;
+	for (unsigned x = 0; x < width; x++) {
+		for (unsigned y = 0; y < height; y++) {
+			FreeImage_SetPixelColor(dib, x, y, &blue);
+			if (y == dot[x] && dot[x] != 0){
+				FreeImage_SetPixelColor(dib, x, y, &black);
+				FreeImage_SetPixelColor(dib, x-1, y, &black);
+				FreeImage_SetPixelColor(dib, x, y-1, &black);
+				FreeImage_SetPixelColor(dib, x-1, y-1, &black);
+			}
 		}
-		if (i == 3000){
-			cout << loc << " " << endl;
-			cout << line.substr(0, loc).c_str() << " " << line.substr(loc + 1).c_str() << endl;
-			cout << left << " " << right << endl;
-		}
-		avg.push_back((left + right) / 2);
-	}*/
-	FIBITMAP *dib = FreeImage_Load(FIF_BMP, "asdf.png", BMP_DEFAULT);
+	}
+}
+
+int countzeros(int dots[1000]){
+	int count = 0;
+	for (int i = 0; i < 1000; i++)
+	if (dots[i] == 0)
+		count++;
+	return count;
+}
+
+int main(){
+	ifstream wavfile("../../../sistar.wav", ios::binary);
+	int i = 0;
+	char buffer[4];
+	for (int j = 0; j < 22; j++){	//Wav header
+		wavfile.read(buffer, 2);
+	}
+
+	FIBITMAP *dib = FreeImage_Load(FIF_PNG, "../../../asdf.png", PNG_DEFAULT);
+	int width = FreeImage_GetWidth(dib);
+	int height = FreeImage_GetHeight(dib);
+
+	/*
 	// Calculate the number of bytes per pixel (3 for 24-bit or 4 for 32-bit)
-	cout << "adsf" << endl;
-	int bytespp = FreeImage_GetLine(dib) / 25362;
-	cout << "adsf" << endl;
-	cout << FreeImage_GetHeight(dib) << " " << 25362 << " " << bytespp << endl;
-	for (unsigned y = 0; y < FreeImage_GetHeight(dib); y++) {
+	int bytespp = FreeImage_GetLine(dib) / FreeImage_GetWidth(dib);
+	for (unsigned y = 0; y < height; y++) {
 		BYTE *bits = FreeImage_GetScanLine(dib, y);
-		for (unsigned x = 0; x < 25362; x++) {
+		for (unsigned x = 0; x < width; x++) {
 			// Set pixel color to green with a transparency of 128
-			/*bits[FI_RGBA_RED] = 0;
+			bits[FI_RGBA_RED] = 0;
 			bits[FI_RGBA_GREEN] = 255;
 			bits[FI_RGBA_BLUE] = 0;
 			bits[FI_RGBA_ALPHA] = 128;
 			// jump to next pixel
-			bits += bytespp;*/
+			bits += bytespp;
 		}
+	}*/
+
+
+	while (wavfile.read(buffer, 4)){	//Skip buffer to...
+		i++;
+		if (abs(getnum(buffer[0], buffer[1])) >= 200){
+			cout << "break at " << i << endl;
+			break;
+		}
+		if (i == 150000)
+			break;
 	}
 
+	int dot[1000];
+	i = 0;
+	while (wavfile.read(buffer, 4)){
+		dot[i] = (getnum(buffer[0], buffer[1]) + 32767) * 640 / 65536;
+		if (abs(dot[i] - 320) < 4){
+			dot[i] = 0;
+		}
+		i++;
+		if (i == 1000)
+			break;
+	}
+	//fillcurve(dot);
+	cout << countzeros(dot) << endl;
+
+
+	/*
+	for (unsigned x = 0; x < width; x++) {
+		for (unsigned y = 0; y < height; y++) {
+			FreeImage_SetPixelColor(dib, x, y, &blue);
+			if (y == dot[x] && dot[x] != 0)
+				FreeImage_SetPixelColor(dib, x, y, &black);
+		}
+	}*/
+	drawCurve(dib, dot, width, height);
+
+
+	HDC hDC = GetDC(NULL);
+	RECT rcDest;
+	rcDest.left = 0;
+	rcDest.top = 0;
+	rcDest.right = 1000;
+	rcDest.bottom = 640;
+
+	SetStretchBltMode(hDC, COLORONCOLOR);
+	StretchDIBits(hDC, rcDest.left, rcDest.top,
+		rcDest.right - rcDest.left, rcDest.bottom - rcDest.top,
+		0, 0, FreeImage_GetWidth(dib), FreeImage_GetHeight(dib),
+		FreeImage_GetBits(dib), FreeImage_GetInfo(dib), DIB_RGB_COLORS, SRCCOPY);
+
 	FreeImage_Unload(dib);
+
 	return 0;
 }

399dea3e81833e4396d66afbc83599f083308257
wikemang <mikescorpian@hotmail.com>
2014-11-19 16:41:29 -0500

    Stuff

--- source/ConsoleApplication1/ConsoleApplication2/Main.cpp
+++ source/ConsoleApplication1/ConsoleApplication2/Main.cpp
@@ -4,8 +4,31 @@
 #include <vector>
 #include <windows.h>
 #include "FreeImage.h"
+#include <time.h>
 
 using namespace std;
+
+const int compression = 64;
+RGBQUAD blue;
+RGBQUAD black;
+RGBQUAD red;
+
+void initColors(){
+	blue.rgbBlue = 255;
+	blue.rgbGreen = 100;
+	blue.rgbRed = 100;
+	blue.rgbReserved = 125;
+
+	black.rgbBlue = 0;
+	black.rgbGreen = 0;
+	black.rgbRed = 0;
+	black.rgbReserved = 125;
+
+	red.rgbBlue = 80;
+	red.rgbGreen = 80;
+	red.rgbRed = 200;
+	red.rgbReserved = 125;
+}
 
 short getnum(char lsb, char msb){
 	short num = (msb << 8) | lsb;
@@ -37,16 +60,6 @@ void fillcurve(int dots[1000]){
 }
 
 void drawCurve(FIBITMAP *dib, int dot[1000], int width, int height){
-	RGBQUAD blue;
-	blue.rgbBlue = 255;
-	blue.rgbGreen = 100;
-	blue.rgbRed = 100;
-	blue.rgbReserved = 125;
-	RGBQUAD black;
-	black.rgbBlue = 0;
-	black.rgbGreen = 0;
-	black.rgbRed = 0;
-	black.rgbReserved = 125;
 	for (unsigned x = 0; x < width; x++) {
 		for (unsigned y = 0; y < height; y++) {
 			FreeImage_SetPixelColor(dib, x, y, &blue);
@@ -60,6 +73,41 @@ void drawCurve(FIBITMAP *dib, int dot[1000], int width, int height){
 	}
 }
 
+
+void drawWave(FIBITMAP *dib, int dot[100000], int width, int height){
+	int i = 0;
+	int lowest, highest;
+	for (unsigned x = 0; x < width; x++) {
+		i += compression;
+		lowest = 700;
+		highest = -300;
+		for (int j = 0; j < compression; j++){
+			if (dot[i + j] != 0){
+				if (dot[i + j] <= lowest)
+					lowest = dot[i + j];
+				if (dot[i + j] >= highest)
+					highest = dot[i + j];
+			}
+		}
+
+		for (unsigned y = 0; y < height; y++) {
+			FreeImage_SetPixelColor(dib, x, y, &blue);
+		}
+		if (lowest != 700 && highest != -300){
+			for (unsigned y = lowest; y < highest; y++) {
+				FreeImage_SetPixelColor(dib, x, y, &black);
+			}
+		}
+	}
+
+	for (unsigned x = 200; x < 204; x++) {
+		for (unsigned y = 0; y < height; y++) {
+			FreeImage_SetPixelColor(dib, x, y, &red);
+		}
+	}
+}
+
+
 int countzeros(int dots[1000]){
 	int count = 0;
 	for (int i = 0; i < 1000; i++)
@@ -69,6 +117,7 @@ int countzeros(int dots[1000]){
 }
 
 int main(){
+	initColors();
 	ifstream wavfile("../../../sistar.wav", ios::binary);
 	int i = 0;
 	char buffer[4];
@@ -80,6 +129,14 @@ int main(){
 	int width = FreeImage_GetWidth(dib);
 	int height = FreeImage_GetHeight(dib);
 
+	HDC hDC = GetDC(NULL);
+	RECT rcDest;
+	rcDest.left = 0;
+	rcDest.top = 0;
+	rcDest.right = 1000;
+	rcDest.bottom = 640;
+
+	SetStretchBltMode(hDC, COLORONCOLOR);
 	/*
 	// Calculate the number of bytes per pixel (3 for 24-bit or 4 for 32-bit)
 	int bytespp = FreeImage_GetLine(dib) / FreeImage_GetWidth(dib);
@@ -98,16 +155,18 @@ int main(){
 
 
 	while (wavfile.read(buffer, 4)){	//Skip buffer to...
+		break;
 		i++;
-		if (abs(getnum(buffer[0], buffer[1])) >= 200){
+		if (abs(getnum(buffer[0], buffer[1])) >= 15){
 			cout << "break at " << i << endl;
 			break;
 		}
-		if (i == 150000)
-			break;
 	}
 
-	int dot[1000];
+	int* dot = new int[1200000];
+	for (int i = 0; i < 1200000; i++){
+		dot[i] = 0;
+	}
 	i = 0;
 	while (wavfile.read(buffer, 4)){
 		dot[i] = (getnum(buffer[0], buffer[1]) + 32767) * 640 / 65536;
@@ -115,8 +174,21 @@ int main(){
 			dot[i] = 0;
 		}
 		i++;
-		if (i == 1000)
+		if (i == 1000000)
 			break;
+	}
+	clock_t  clock1, clock2;
+	for (int jk = 0; jk < 10000; jk++){
+		clock1 = clock();
+		dot+= compression * 7;
+		drawWave(dib, dot, width, height);
+		StretchDIBits(hDC, rcDest.left, rcDest.top,
+			rcDest.right - rcDest.left, rcDest.bottom - rcDest.top,
+			0, 0, FreeImage_GetWidth(dib), FreeImage_GetHeight(dib),
+			FreeImage_GetBits(dib), FreeImage_GetInfo(dib), DIB_RGB_COLORS, SRCCOPY);
+		clock2 = clock();
+		float diff = (((float)clock2 - (float)clock1) / 1000000.0F) * 1000;
+		//Sleep(10 - diff);
 	}
 	//fillcurve(dot);
 	cout << countzeros(dot) << endl;
@@ -130,22 +202,9 @@ int main(){
 				FreeImage_SetPixelColor(dib, x, y, &black);
 		}
 	}*/
-	drawCurve(dib, dot, width, height);
 
 
-	HDC hDC = GetDC(NULL);
-	RECT rcDest;
-	rcDest.left = 0;
-	rcDest.top = 0;
-	rcDest.right = 1000;
-	rcDest.bottom = 640;
-
-	SetStretchBltMode(hDC, COLORONCOLOR);
-	StretchDIBits(hDC, rcDest.left, rcDest.top,
-		rcDest.right - rcDest.left, rcDest.bottom - rcDest.top,
-		0, 0, FreeImage_GetWidth(dib), FreeImage_GetHeight(dib),
-		FreeImage_GetBits(dib), FreeImage_GetInfo(dib), DIB_RGB_COLORS, SRCCOPY);
-
+	delete(dot);
 	FreeImage_Unload(dib);
 
 	return 0;

fb30d0f4003cd000ebe59074debeede9d4d2226f
wikemang <mikescorpian@hotmail.com>
2014-11-21 15:57:04 -0500

    stuff

---
+++ source/ConsoleApplication1/ConsoleApplication2/PlayFile.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <dshow.h>
+#include <cstdio>
+
+using namespace std;
+// For IID_IGraphBuilder, IID_IMediaControl, IID_IMediaEvent
+#pragma comment(lib, "strmiids.lib") 
+
+// Obviously change this to point to a valid mp3 file.
+const wchar_t* filePath = L"../../../sistar.wav";
+void asdf(){
+	cout << "asdfqwe" << endl;
+}
